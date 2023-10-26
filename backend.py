448494b7333ac8f00590341974fb98ff864db88c
Kartik Neti <kartikprajak@gmail.com>
2023-10-26 10:51:14 +0530

    Final MVP

--- backend.py
+++ backend.py
@@ -3,20 +3,23 @@ import requests, json
 
 api_key = "8b8218bb7e0feffe5ace6189f7b743e2"
 
-def get_data(place,days=None,kind=None):
+def get_data(place,days=None):
     
-    place = 'tokyo'
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}'
     response = requests.get(url)
     data = response.json()
+    filtered_data = data["list"]
+    if filtered_data:
+        nr_values = 8 * days
+        filtered_data = filtered_data[:nr_values]
     
-    for i in data["list"][:5]:
-        days5 = i["main"]["temp"]
         
-    
-    return data
+        return filtered_data
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    print(get_data(place= "tokyo"))
+    # print(get_data(place= "tokyo",days=3,kind="Temperature"))
+    get_data()
 
--- main.py
+++ main.py
@@ -12,9 +12,22 @@ option = st.selectbox("Select data to view:", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for next {days} days in {place}")
 
+if place:
+    try:
+        filtered_data = get_data(place,days) 
+        
 
-
-data = get_data(place,days,kind)  
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temp (C)"} )
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [temp["main"]["temp"]/10 for temp in filtered_data]
+            dates = [temp["dt_txt"] for temp in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temp (C)"} )
+            st.plotly_chart(figure)
+            
+        if option == "Sky":
+            image = {"Clear": "images/clear.png","Clouds": "images/cloud.png","Rain": "images/rain.png","Snow": "images/snow.png"}
+            sky_condition = [temp["weather"][0]["main"] for temp in filtered_data]
+            image_paths = [image[condition] for condition in sky_condition]
+            st.image(image_paths, width=115)
+            
+    except:
+        st.subheader(f"{place} does not exist.")
